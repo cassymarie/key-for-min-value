fd0831c266fd82237f826ae89437337686273587
cassymarie <cassy2205@gmail.com>
2020-10-22 10:22:05 -0600

    commit tree b24d8c1e756b426df309eaa99549164b7e96158b

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,19 @@
 
 def key_for_min_value(name_hash)
 
+    if name_hash.size < 1 
+         puts "empty hash" 
+    else 
+        smallest_name = ""
+        lowest_value = 0
+        count = 0
+        name_hash.each do |name, amount|
+            if count < 1 || amount.to_i < lowest_value.to_i
+                lowest_value = amount.to_i
+                smallest_name = name
+            end
+            count += 1
+        end
+        return smallest_name
+    end
 end
